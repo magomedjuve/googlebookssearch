7d87be183adf8c18a675eedd6c8ccff9c07a4e15
Magomed <83663832+magomedjuve@users.noreply.github.com>
2021-07-13 17:39:30 +0300

    booksearch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import Country from './components/country';
+// import GooglePexel from "./googlePexel";
+import GoogleBooksSearch from "./googleBooksSearch";
+// import Contacts from './components/contacts';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <GooglePexel></GooglePexel>
+    <GoogleBooksSearch></GoogleBooksSearch>
+    // <div className="row">
+    //   <div className="col-md-8 offset-md-2">
+    //     {/* <Contacts /> */}
+    //     <Country />
+    //   </div>
+    // </div>
+  )
 }
 
 export default App;
